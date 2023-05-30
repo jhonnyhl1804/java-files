c9d59169fbe54b2b08932c9ddacd0a4d71bb0f79
Jhonnyhl <jhonnyhoyoslopez@gmail.com>
2023-05-29 23:25:33 -0500

    mi app en java

---
+++ src/zeus_prj/Zeus_prj.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zeus_prj;
+
+/**
+ *
+ * @author asus
+ */
+public class Zeus_prj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FrmMascotas f = new FrmMascotas();
+       f.setLocationRelativeTo( null );
+       f.setVisible(true);
+       /*
+        // Declarar
+        ClsMascotas mas;
+        //Inicializar el objeto
+        mas = new ClsMascotas();
+        
+        mas.identificacion = 123;
+        mas.apodo = "carlos";
+        mas.nombres = "michi";
+        mas.sexo = "hembra";
+       //Mostrar los datos
+        System.out.println("identificación: "+mas.identificacion);
+        System.out.println("edad: "+mas.apodo);
+        System.out.println("nombre: "+mas.nombres);
+        System.out.println("sexo: "+mas.sexo);
+       */ 
+        
+    }
+    
+}
